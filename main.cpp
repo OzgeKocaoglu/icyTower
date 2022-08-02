8683e94796d6eda6003626ba27cf0efefd847a57
Özge Kocaoğlu <ozge.kocaoglu@outlook.com>
2022-08-02 22:58:03 +0300

    frame rate fixed

--- main.cpp
+++ main.cpp
@@ -3,13 +3,31 @@
 Game* game = __nullptr;
 
 int main(int argc, char* args[]) {
+
+	const int fps = 60;
+	const int frameDelay = 1000 / fps;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("My game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, false);
+
 	while(game->running())
 	{
+
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if(frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
